b8f4d61e3fff2f652cf271515ae17c7407e8c41c
arkainoh <arkainoh@gmail.com>
2017-04-06 21:13:36 +0900

    add Vocabulary class

--- src/Tools.py
+++ src/Tools.py
@@ -1,5 +1,37 @@
 import numpy as np
-
 # implement Softmax, Negative Sampling and other useful functions
-class Tools:
-    
+
+
+# Set of vocabularies with indices
+class Vocabulary:
+	def __init__(self):
+		self.vector = {}
+
+	def add(self, tokens):
+		for token in tokens:
+			if token not in self.vector:
+				self.vector[token] = len(self.vector)
+
+	def indexOf(self, vocab):
+		return self.vector[vocab]
+
+	def size(self):
+		return len(self.vector)
+
+	# vectorize = dict -> numpy.array
+	def vectorize(self, word):
+		v = [0 for i in range(self.size())]
+		if word in self.vector:
+			v[self.indexOf(word)] = 1
+		else:
+			print("<ERROR> Word \'" + word + "\' Not Found")
+		return np.array(v)
+
+	def __str__(self):
+		s = "Vocabulary("
+		for word in self.vector:
+			s += (str(self.vector[word]) + ": " + word + ", ")
+		if self.size() != 0:
+			s = s[:-2]
+		s += ")"
+		return s
