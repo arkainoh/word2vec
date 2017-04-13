f0eef5d23c956af994b2fa8b98594eca84749ab5
arkainoh <arkainoh@gmail.com>
2017-04-14 02:39:06 +0900

    start transforming one-word context model into CBOW model, fix initialization part and structure of the model itself

--- src/Models.py
+++ src/Models.py
@@ -5,15 +5,17 @@ import nltk
 # implement CBOW, Skip-Gram
 class CBOW:
 
-    def __init__(self, C, N):
+    def __init__(self, C, dimension, voc):
         self.W_i = np.empty(0)
-        self.W_i = np.empty(0)
+        self.W_o = np.empty(0)
         self.C = C
-        self.N = N
-        self.V = 0
+        self.N = dimension
         self.voc = Tools.Vocabulary()
+        self.voc.load(voc)
+        self.V = self.voc.size()
+        self.words = []
 
-    def setTrainingData(self, filename):
+    def addTrainingData(self, filename):
         inputstr = ""
         f = open(filename)
         lines = f.readlines()
@@ -23,12 +25,20 @@ class CBOW:
         tokens = nltk.word_tokenize(inputstr)
         tokens = [i for i in tokens if i.isalpha()]
         self.voc.add(tokens)
+        self.words.append(tokens)
         self.V = self.voc.size()
+
+    def init_weights():
         self.W_i = np.random.rand(self.V, self.N)
         self.W_o = np.random.rand(self.N, self.V)
     
     def train(self, learning_rate):
         for i in range(self.V):
+
+            # collect C words
+            # self.W_i[i] = avg(C words)
+
+            t = self.voc.vectorize(self.voc.at(i)) # target word
 
             # h = self.W_i[i]
             u = np.matmul(self.W_i[i], self.W_o)
@@ -36,7 +46,6 @@ class CBOW:
             
             # update W_o
             for j in range(self.V):
-                t = self.voc.vectorize(self.voc.at(j))
                 e = y - t
                 self.W_o[:, j] -= learning_rate * e[j] * self.W_i[i]
             
@@ -44,6 +53,3 @@ class CBOW:
             EH = np.matmul(e, self.W_o.T)
             self.W_i[i] -= learning_rate * EH
 
-test = CBOW(C = 5, N = 3)
-test.setTrainingData('test.txt')
-test.train(0.5)
