9213ab485712dd21f4d6138fefcd807293281e01
arkainoh <arkainoh@gmail.com>
2017-05-22 01:40:30 +0900

    complete huffman tree

--- src/Tools.py
+++ src/Tools.py
@@ -1,11 +1,15 @@
 import numpy as np
 import math
 import heapq
+
 # implement Softmax, Negative Sampling and other useful functions
 
 def softmax(vec):
     m = np.max(vec)
     return np.exp(vec - m) / np.exp(vec - m).sum(axis = 0)
+
+def sigmoid(x):
+    return 1 / (1 + np.exp(-x))
     
 def cosSimilarity(A, B):
     multi = (A.dot(B))
@@ -72,7 +76,7 @@ class Vocabulary:
         return s
 
 class TreeNode:
-    def __init__(self, word='', data=None, left=None, right=None, weight=0):
+    def __init__(self, word = '', data = None, left = None, right = None, weight = 0):
         self.word = word
         self.data = data
         self.left = left
@@ -86,54 +90,82 @@ class TreeNode:
         return self.weight <= other.weight
 
 class HuffmanTree:
-    def __init__(self, dimension, root=None):
+    def __init__(self, dimension, tokfreq = None):
         self.dimension = dimension
         self.root = None
+        self.path = {}
+        if(tokfreq):
+            self.build(tokfreq)
+
     def build(self, tokfreq): # build Huffman tree with given term frequencies
-        pq = [TreeNode(word=key, weight=tokfreq[key]) for key in tokfreq]
+        pq = []
+        for key in tokfreq:
+            pq.append(TreeNode(word=key, weight=tokfreq[key]))
+            self.path[key] = []
+
         heapq.heapify(pq)
         while(len(pq) >= 2):
             tmpR = heapq.heappop(pq)
             tmpL = heapq.heappop(pq)
-            heapq.heappush(pq, TreeNode(data=np.random.rand(self.dimension), left=tmpL, right=tmpR, weight=tmpR.weight + tmpL.weight))
+            tmpP = TreeNode(data = np.random.rand(self.dimension), left = tmpL, right = tmpR, weight = tmpR.weight + tmpL.weight)
+
+            # this algorithm seems inefficient... it should be replaced later
+            for word in self.words(tmpR):
+                self.path[word].append(0)
+            for word in self.words(tmpL):
+                self.path[word].append(1)
+
+            heapq.heappush(pq, tmpP)
         self.root = pq[0]
+        for key in self.path:
+            self.path[key].reverse()
 
-def sigmoid (x): return 1 / (1 + np.exp(-x))
+    def nodes(self, node = None): # inorder traversal
+        l = []
+        stack = []
+        if(node):
+            curNode = node
+        else:
+            curNode = self.root
+        while(len(stack) or curNode is not None):
+            if curNode is not None:
+                stack.append(curNode)
+                curNode = curNode.left
+            else:
+                curNode = stack.pop()
+                l.append(curNode)
+                curNode = curNode.right
+        return l
 
-def findPath(number, v):
-    n = int(math.log(v, 2))
-    a = []
-    if number <= 2 * (v - 2 ** n):
-        num = n
-    else :
-        number = number - (v - 2 ** n) 
-        num = n - 1
-    while num >= 0:
-        a.append(int(number / (2 ** num)))
-        number = number % (2 ** num)
-        num -= 1
-    return a
+    def words(self, node = None): # inorder traversal
+        l = []
+        stack = []
+        if(node):
+            curNode = node
+        else:
+            curNode = self.root
+        while(len(stack) or curNode is not None):
+            if curNode is not None:
+                stack.append(curNode)
+                curNode = curNode.left
+            else:
+                curNode = stack.pop()
+                if curNode.word:
+                    l.append(curNode.word)
+                curNode = curNode.right
+        return l
 
-def jaemin():
+    # j starts with 0 in n(w, j) and t(w, j)
+    def n(self, word, j):
+        if j > len(self.path[word]):
+            return None
+        curNode = self.root
+        for i in range(j):
+            if self.path[word][i]: # left
+                curNode = curNode.left
+            else:
+                curNode = curNode.right
+        return curNode
 
-    v = 100  # voca size
-    n = 50 # hidden dimension
-    t_word = 72 #실제 out이 되어야하는 word의 번호
-
-    h = np.random.rand(n, 1)
-    w_o = np.random.rand(n, v)
-
-    a = findPath(t_word, v)
-    print(a)
-
-    n_th = 0
-    for i in a :
-        if i == 0 :
-            w_o[:, n_th] = w_o[:, n_th] - 0.2 * (sigmoid((w_o.transpose()[n_th].dot(h))[0]) - 1) * h[:, 0]
-            print(n_th)
-            n_th = n_th * 2 + 1
-        else :
-            w_o[:, n_th] = w_o[:, n_th] - 0.2 * (sigmoid((w_o.transpose()[n_th].dot(h))[0])) * h[:, 0]
-            print(n_th)
-            n_th = n_th * 2 + 2
-
+    def t(self, word, j):
+        return self.path[word][j]
