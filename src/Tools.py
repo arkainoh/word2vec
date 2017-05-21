af76ae81e44ee390acdff07da2fd8e95d62daf3b
arkainoh <arkainoh@gmail.com>
2017-05-21 21:51:41 +0900

    implement huffman tree

--- src/Tools.py
+++ src/Tools.py
@@ -1,17 +1,27 @@
 import numpy as np
 import math
+import heapq
 # implement Softmax, Negative Sampling and other useful functions
 
 def softmax(vec):
     m = np.max(vec)
     return np.exp(vec - m) / np.exp(vec - m).sum(axis = 0)
-
+    
 def cosSimilarity(A, B):
     multi = (A.dot(B))
     x = math.sqrt(A.dot(A))
     y = math.sqrt(B.dot(B))
     result = multi / (x * y)
     return result
+
+def tokFreq(tokens):
+    dic = {}
+    for token in tokens:
+        if token in dic:
+            dic[token] += 1
+        else:
+            dic[token] = 1
+    return dic
 
 # Set of vocabularies with indices
 class Vocabulary:
@@ -61,10 +71,32 @@ class Vocabulary:
         s += ")"
         return s
 
-"""
-Created on Thu Apr  6 20:01:51 2017
-@author: JAEMIN
-"""
+class TreeNode:
+    def __init__(self, word='', data=None, left=None, right=None, weight=0):
+        self.word = word
+        self.data = data
+        self.left = left
+        self.right = right
+        self.weight = weight
+
+    def __lt__(self, other):
+        return self.weight < other.weight
+
+    def __le__(self, other):
+        return self.weight <= other.weight
+
+class HuffmanTree:
+    def __init__(self, dimension, root=None):
+        self.dimension = dimension
+        self.root = None
+    def build(self, tokfreq): # build Huffman tree with given term frequencies
+        pq = [TreeNode(word=key, weight=tokfreq[key]) for key in tokfreq]
+        heapq.heapify(pq)
+        while(len(pq) >= 2):
+            tmpR = heapq.heappop(pq)
+            tmpL = heapq.heappop(pq)
+            heapq.heappush(pq, TreeNode(data=np.random.rand(self.dimension), left=tmpL, right=tmpR, weight=tmpR.weight + tmpL.weight))
+        self.root = pq[0]
 
 def sigmoid (x): return 1 / (1 + np.exp(-x))
 
